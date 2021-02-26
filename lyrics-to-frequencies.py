4c61f81c9ee3c0b8a4fa273a9e6a909d8b802156
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-02-26 13:52:50 +0100

    Add most_common_words function

--- lyrics-to-frequencies.py
+++ lyrics-to-frequencies.py
@@ -6,3 +6,14 @@ def lyrics_to_frequencies(lyrics):
         else:
             myDict[word] = 1
     return myDict
+
+
+def most_common_words(freqs):
+    values = freqs.values()
+    best = max(values)
+    words = []
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+    return words, best
+
