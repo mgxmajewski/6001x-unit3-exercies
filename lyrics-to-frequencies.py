d8f83bc3311990f9034bddd748e5184c62f5bbfe
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-02-26 14:03:01 +0100

    Add words_often function

--- lyrics-to-frequencies.py
+++ lyrics-to-frequencies.py
@@ -17,3 +17,17 @@ def most_common_words(freqs):
             words.append(k)
     return words, best
 
+def words_often(freqs, minTimes):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+    return result
+
+
